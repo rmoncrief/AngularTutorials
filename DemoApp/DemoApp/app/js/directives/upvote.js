916631d2c8f52acd4d0e925f98a87c186d252531
rmoncrief <rmoncri1@vols.utk.edu>
2016-08-02 10:12:06 -0400

    changed the upvote directives

---
+++ DemoApp/DemoApp/app/js/directives/upvote.js
@@ -0,0 +1,13 @@
+'use strict';
+eventsApp.directive('upvote', function() {
+  return {
+    restrict: 'E',
+    // replace: true,
+    templateUrl: '/templates/directives/upvote.html',
+    scope: { 
+      upvote: "&",
+      downvote: "&",
+      count: "="
+    }
+  }
+});
